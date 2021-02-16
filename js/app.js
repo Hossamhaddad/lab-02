1fdc4e6dad0e32bd8274399d591cd011c38f36a7
hossamhaddad <husamhaddad80@gmail.com>
2021-02-16 18:44:39 +0200

    lab3

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 'use strict';
-$.ajax('../data/page-1.json')
+$.ajax('data/page-1.json')
     .then(data => {
-    data.forEach(element => {
-    let newImage = new TemplateImages(element);
-    newImage.render();
-    if (!allKeyword.includes(element.keyword)) {
-        allKeyword.push(element.keyword);
-    }
+        data.forEach(element => {
+            let newImage = new TemplateImages(element);
+           let render=newImage.render();
+           $('main').append(render);
+            if (!allKeyword.includes(element.keyword)) {
+                allKeyword.push(element.keyword);
+            }
+     
+        });
+        select();
+
+});
+
+let allImg2=[];
+$.ajax('data/page-2.json')
+    .then(data => {
+        data.forEach(element => {
+        allImg2.push(element)
+        if (!allKeyword2.includes(element.keyword)) {
+            allKeyword2.push(element.keyword);
+        }
+        });
+      
+});
 
 
-    });
-    select();
 
-    });
-let allImg=[];
+// let allKeyword2 = [];
+let allImg = [];
 let allKeyword = [];
 function TemplateImages(value) {
     this.title = value.title;
@@ -21,37 +37,90 @@ function TemplateImages(value) {
     this.description = value.description;
     this.keyword = value.keyword;
     this.horns = value.horns;
-   allImg.push(this);
+    allImg.push(this);
 }
 
 TemplateImages.prototype.render = function () {
-    
-    let imageClone = $('#photo-template').clone();
-    imageClone.removeAttr('id');
-    imageClone.find('h2').text(this.title);
-    imageClone.find('img').attr('src', this.image_url);
-    imageClone.find('p').text(this.description);
-    $('main').append(imageClone);
-}
+    let template = $('#imagesTem').html();
+    let newObj = Mustache.render(template, this);
+    return newObj;
+};
 
 function select() {
     allKeyword.forEach(each => {
 
-    $('select').append(`<option> ${each}</option>`);
-
-
+        $('#select').append(`<option> ${each}</option>`);
+        
     });
 }
-$('#select').on('change',function(){
-$('main').html('');
-$('main').html('<div id="photo-template"> <h2></h2> <img src="" alt=""> <p></p></div>') ;
-allImg.forEach(item=>{
- if(item.keyword==$('select').val() ){
-     
-    let newImage = new TemplateImages(item) ; 
+
+// function select2() {
+//     allKeyword2.forEach(each => {
+       
+//         $('#select').append(`<option> ${each}</option>`);
+       
+//     });
+// }
+
+function sort(){
+    $('#sort').append('<option> title </option>');
+    $('#sort').append('<option> horns </option>');
    
-    newImage.render() ; 
-   
- }
+}
+sort();
+
+$('#select').on('change', function () {
+    $('main').html('');
+    allImg.forEach(item => {
+        if (item.keyword == $('#select').val()) {
+
+            let newImage = new TemplateImages(item);
+            let render=newImage.render();
+            if (allImg.includes(newImage)) {
+                allImg.pop(newImage);
+            }
+            
+            $('main').append(render);
+
+        }
+    })
 })
+
+
+
+$('#page1').on('click',function(){
+    
+    $('main').html('');
+    allImg.forEach(item => {
+            let newImage = new TemplateImages(item);
+            let render=newImage.render();
+            $('main').append(render);
+            if (allImg.includes(newImage)) {
+                allImg.pop(newImage);
+            }
+        
+    })
 })
+
+$('#page2').on('click',function(){
+    
+    $('main').html('');
+    allImg2.forEach(item => {
+            let newImage = new TemplateImages(item);
+            let render=newImage.render();
+            $('main').append(render);
+            if(allImg.includes(newImage)){
+                allImg.pop(newImage);
+            }
+
+       select2();
+    })
+})
+
+$('#sort').on('change',function () {
+        if ($('#sort').val()=='horns') {
+            alert('hi')
+        }else if ($('#sort').val()=='title'){
+            alert('bye')
+        }
+    })
